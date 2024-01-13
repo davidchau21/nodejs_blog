622b63016fe517ca7f03bc1dfc0a92a3d05e1b5d
davidchau21 <davidduongxu1@gmail.com>
2024-01-13 14:15:18 +0700

    morgan

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 
-app.get('/tin-tuc', (req, res) => {
-    var a = 1;
-    var b = 2;
-    var c = a + b;
+app.use(morgan('combined'))
+
+app.get('/', (req, res) => {
+
   res.send('Hello World!')
 })
 
