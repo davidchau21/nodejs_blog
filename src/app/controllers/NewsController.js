91dfdacd42d9df239434d0175190a9bf4afe115f
davidchau21 <davidduongxu1@gmail.com>
2024-01-16 21:28:05 +0700

    refactor: MVC

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,13 @@
+class NewController {
+    // [GET] /news
+    index(req, res) {
+        res.render('home');
+    }
+
+    // [GET] /:slug
+    show(req, res) {
+        res.send('New Detail')
+    }
+}
+
+module.exports = new NewController();
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routers');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.urlencoded({
@@ -23,24 +25,9 @@ app.engine('hbs', handlebars.engine({extname: '.hbs'}));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resource/views'));
 
+// route init 
+route(app);
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
-
-
-app.get('/news', (req, res) => {
-  res.render('news');
-});
-
-app.get('/search', (req, res) => {
-  res.render('search');
-});
-
-app.post('/search', (req, res) => {
-  console.log(req.body);
-  res.send('');
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
