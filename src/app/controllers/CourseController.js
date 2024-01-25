d6b515166f418c472b43fb21a160d4ebe783c823
davidchau21 <davidduongxu1@gmail.com>
2024-01-25 14:09:40 +0700

    delete course

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -44,6 +44,13 @@ class CourseController {
     .catch(next);
   }
 
+  // [DELETE] /courses/:id
+  destroy(req, res, next) {
+    Course.deleteOne({_id: req.params.id})
+    .then(() => res.redirect('back'))
+    .catch(next);
+  }
+
 }
 
 module.exports = new CourseController();
