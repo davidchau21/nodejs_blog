b7f9978f1c6b92274f3c7ce6265d4594c255ce11
davidchau21 <davidduongxu1@gmail.com>
2024-01-31 20:58:40 +0700

    deletecount con loi

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -4,6 +4,21 @@ const {mutipleMongooseToObject} = require('../../util/mongoose');
 class MeController {
   // [GET] /me/stored/courses
   storedCourses(req, res, next) {
+    Promise.all([ Course.find({}), Course.countDocumentsDeleted()])
+    .then(([courses, deleteCount]) =>
+      res.render("me/stored-courses", {
+        deleteCount,
+        courses: mutipleMongooseToObject(courses),
+      })
+    )
+    .catch(next);
+
+    Course.countDocumentsDeleted()
+    .then((deleteCount) => {
+      console.log(deleteCount);
+    })
+    .catch(() => {});
+
     Course.find({})
     .then(courses => res.render("me/stored-courses", {
       courses: mutipleMongooseToObject(courses)
