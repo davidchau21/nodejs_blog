5649da89cdba19abaaa1a2e5c0529bf7e105b69f
davidchau21 <davidduongxu1@gmail.com>
2024-02-22 21:24:08 +0700

    select all

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -1,40 +1,55 @@
 const Course = require("../models/Course");
-const {mutipleMongooseToObject} = require('../../util/mongoose');
+const { mutipleMongooseToObject } = require("../../util/mongoose");
 
 class MeController {
   // [GET] /me/stored/courses
   storedCourses(req, res, next) {
-    Promise.all([ Course.find({}), Course.countDocumentsDeleted()])
-    .then(([courses, deleteCount]) =>
-      res.render("me/stored-courses", {
-        deleteCount,
-        courses: mutipleMongooseToObject(courses),
-      })
-    )
-    .catch(next);
+    Promise.all([Course.find({}), Course.countDocumentsDeleted()])
+      .then(([courses, deleteCount]) =>
+        res.render("me/stored-courses", {
+          deleteCount,
+          courses: mutipleMongooseToObject(courses),
+        })
+      )
+      .catch(next);
 
     Course.countDocumentsDeleted()
-    .then((deleteCount) => {
-      console.log(deleteCount);
-    })
-    .catch(() => {});
+      .then((deleteCount) => {
+        // console.log(deleteCount);
+      })
+      .catch(() => { });
 
-    Course.find({})
-    .then(courses => res.render("me/stored-courses", {
-      courses: mutipleMongooseToObject(courses)
-    }))
-    .catch(next);
-    
+    // Course.find({})
+    // .then(courses => res.render("me/stored-courses", {
+    //   courses: mutipleMongooseToObject(courses)
+    // }))
+    // .catch(next);
   }
 
   // [GET] /me/trash/courses
   trashCourses(req , res, next) {
-    Course.findDeleted({})
+    Course.findDeleted({deleted: true})
     .then(courses => res.render("me/trash-courses", {
       courses: mutipleMongooseToObject(courses)
     }))
     .catch(next);
   }
+
+  // trashCourses(req, res, next) {
+  //   Course.findDeleted({ deleted: true })
+  //     .then(deletedCourses => {
+  //       console.log(deletedCourses); // Log deleted courses for debugging
+  //       res.render("me/trash-courses", {
+  //         courses: mutipleMongooseToObject(deletedCourses)
+  //       });
+  //     })
+  //     .catch(error => {
+  //       console.error(error); // Log any errors that occur
+  //       next(error); // Pass the error to the error-handling middleware
+  //     });
+  // }
+
+
 }
 
 module.exports = new MeController();
