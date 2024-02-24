73b3403e1eac2fac2b35d2395e8bfe3e2400cf86
davidchau21 <davidduongxu1@gmail.com>
2024-02-24 19:55:29 +0700

    sort middleware

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const methodOverride = require('method-override');
 const app = express();
 const port = 3000;
 
+const SortMiddleware = require('./app/middlewares/sortMiddleware');
+
 const route = require('./routers');
 const db = require('./config/db');
 
@@ -23,15 +25,42 @@ app.use(express.json());
 
 app.use(methodOverride('_method'));
 
+// Custom middleware
+app.use(SortMiddleware);
+
 //XMLHttpRequest, fetch, axios
 
 // HTTP logger
 // app.use(morgan('combined'))
 
 //Template engine
-app.engine('hbs', handlebars.engine({ extname: '.hbs', helpers: {
-    sum: (a, b) => a + b,
-}}));
+app.engine('hbs', handlebars.engine({
+    extname: '.hbs', helpers: {
+        sum: (a, b) => a + b,
+        sortable: (field, sort) => {
+            const sortType = field === sort.column ? sort.type : 'default';
+
+            const icons = {
+                default: 'fas fa-sort',
+                asc: 'fas fa-sort-amount-down-alt',
+                desc: 'fas fa-sort-amount-down',
+            };
+
+            const types = {
+                default: 'desc',
+                asc: 'desc',
+                desc: 'asc',
+            };
+
+            const icon = icons[sortType];
+            const type = types[sortType];
+
+            return `<a href="?_sort&colunm=${field}&type=${type}">
+            <i class="${icon}"></i>
+          </a>`;
+        }
+    }
+}));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resource', 'views'));
 
